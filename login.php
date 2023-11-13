3d6da35504873ab13ef877ef20e3d7ff9c395bb6
gk-mokaya <kevinmokaya001@gmail.com>
2023-11-13 11:46:48 -0800

    first commit

---
+++ login.php
@@ -0,0 +1,33 @@
+<html>
+<body>
+<?php
+ include 'connect.php';
+/*Use of Sessions*/
+if(!session_id())
+session_start();
+/*simple checking of the data*/
+if(isset($_POST['loginID']) && isset($_POST['password']))
+{
+    $username ="root";
+    $password ="";
+    $hostname ="localhost"; 
+    $database="student";
+    $name=$_POST['loginID'];
+    $password=$_POST['password'];
+    $sql= "SELECT * FROM login WHERE loginID='$name' and password='$password'";  
+    $result=mysqli_query($db,$sql);
+
+     if (mysqli_num_rows($result)>0) 
+         {     
+          echo ("Welcome back  ".$name); 
+          }
+     else
+     {
+       echo ("Wrong Login ID or password");   
+      }
+    }
+    mysqli_close($db);
+    ?>
+    </body>
+    </html>
+    
---
+++ signup.php
@@ -0,0 +1,34 @@
+<html>
+<body>
+<?php
+/*Use of Sessions*/
+ include 'connect.php';
+if(!session_id())
+session_start();
+
+/*simple checking of the data*/
+if(isset($_POST["loginID"]) && isset($_POST["password"]) && ($_POST["password"] == $_POST["confirm"]) && ($_POST["loginID"] !="") && ($_POST["password"]!=""))
+{
+    $username ="root";
+    $password ="";
+    $hostname ="localhost"; 
+    $database="student";
+    $name=$_POST['loginID'];
+    $password=$_POST['password'];
+    $fname=$_POST['fname'];
+    $phone=$_POST['phone'];
+    $sql="INSERT INTO login (loginID,fname,phone,password) VALUES ('$name','$fname','$phone','$password')";
+    mysqli_query($db,$sql);
+   echo ("User added sucessfully");
+}
+   else
+    {
+       //echo (" Password does not match or some fields in the form are not filled");
+       $message = "Password does not match or some fields in the form are not filled";
+       echo "<script type='text/javascript'>alert('$message');</script>";	
+    }
+
+?>
+</body>
+</html>
+       
